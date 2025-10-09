f906dff12e46e3a57c1b016d9df8f835aeeed0e5
San <soltks123@gmail.com>
2025-10-09 20:49:31 +0900

    cloudetype 테스트

--- db.py
+++ db.py
@@ -2,13 +2,24 @@ import pymysql
 import os
 from dotenv import load_dotenv
 
-load_dotenv("apikey.env")
+
+# load_dotenv("apikey.env")
+# def get_conn():
+#     return pymysql.connect(
+#         host=os.getenv("DATABASE_URL"),
+#         user=os.getenv("DATABASE_USER"),
+#         password=os.getenv("DATABASE_PASSWORD"),
+#         db=os.getenv("DATABASE_NAME"),
+#         charset="utf8mb4",
+#         cursorclass=pymysql.cursors.DictCursor
+#     )
+
+# 배포 시
 def get_conn():
     return pymysql.connect(
-        host=os.getenv("DATABASE_URL"),
-        user=os.getenv("DATABASE_USER"),
-        password=os.getenv("DATABASE_PASSWORD"),
-        db=os.getenv("DATABASE_NAME"),
-        charset="utf8mb4",
+        host=os.environ.get("DATABASE_URL"),
+        user=os.environ.get("DATABASE_USER"),
+        password=os.environ.get("DATABASE_PASSWORD"),
+        db=os.environ.get("DATABASE_NAME"),
         cursorclass=pymysql.cursors.DictCursor
     )
--- main.py
+++ main.py
@@ -22,4 +22,4 @@ def index_page(request: Request):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
